fbe6c081e5adeae3e3dafea6c4a41f43d69dc240
mark.glose <bxbitbx@gmail.com>
2024-05-22 17:43:09 -0500

    fixing hostname

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -8,9 +8,7 @@ const HeaderSocials = () => {
     
     <div className="header__socials">
 
-        <a href="https://www.linkedin.com/in/thakshila-bandara-2791b9214" target="_blank" rel='noreferrer'><BsLinkedin/></a>
-        <a href="https://github.com/Thakshila-Bandara" target="_blank" rel='noreferrer'><BsGithub/></a>
-        <a href="https://medium.com/@thakshiladb2000" target="_blank" rel='noreferrer'><BsMedium/></a>
+        <a href="https://www.linkedin.com/in/casper-shepard-9b4848b4/" target="_blank" rel='noreferrer'><BsLinkedin/></a>
     </div>
   )
 }
